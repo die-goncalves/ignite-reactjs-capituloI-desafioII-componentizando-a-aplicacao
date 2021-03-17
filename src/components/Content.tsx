c794d16fe2cc333d727b9495210ef5c3403d065d
die-goncalves <die.goncalves1990@gmail.com>
2021-03-17 10:05:18 -0300

    Criar componente Content e atualizar content.scss

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,39 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+import { MovieCard } from '../components/MovieCard';
+import { api } from '../services/api';
+
+import '../styles/content.scss';
+
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  selectedGenreId: number;
+}
+
+export function Content(props: ContentProps) {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${props.selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+  }, [props.selectedGenreId]);
+
+  return (
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  )
 }
